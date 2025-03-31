f84d3f247d64caf6e5be5a4dd6578fbcf72c7497
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2025-03-31 12:21:15 +0530

    add second experiment

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -7,6 +7,8 @@ url = "https://raw.githubusercontent.com/araj2/customer-database/refs/heads/mast
 df = pd.read_csv(url)
 
 df = df.iloc[:, 3:]
-df = df[df["Length of Membership"] > 3]
+
+df.drop(columns=["Avg. Session Length"], inplace=True)
+df = df[df["Length of Membership"] > 1]
 
 df.to_csv(os.path.join("data", "customers.csv"), index=False)
